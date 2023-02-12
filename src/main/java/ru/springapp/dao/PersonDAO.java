c7c1ae108f2682ddd6776d6cff4add690597fe89
Magdel <savelyzhur98@gmail.com>
2023-02-12 18:01:02 +0600

    Initial commit

---
+++ src/main/java/ru/springapp/dao/PersonDAO.java
@@ -0,0 +1,109 @@
+package ru.springapp.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.springapp.models.Person;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PersonDAO {
+
+    public final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM PEOPLE", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM PEOPLE WHERE id=?", new Object[]{id},
+                        new BeanPropertyRowMapper<>(Person.class))
+                .stream()
+                .findAny().orElse(null);
+    }
+
+    public Optional<Person> show(String email) {
+        return jdbcTemplate.query("SELECT * FROM PEOPLE WHERE email=?", new Object[]{email},
+                        new BeanPropertyRowMapper<>(Person.class))
+                .stream()
+                .findAny();
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO PEOPLE (name, age, email) VALUES( ?, ?, ?)",
+                person.getName(), person.getAge(), person.getEmail());
+    }
+
+    public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update("UPDATE PEOPLE SET name=?, age=?, email=? WHERE id=?",
+                updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM PEOPLE WHERE id=?", id);
+    }
+
+    //Тестируем производительность batch
+
+    public void testMultipleUpdate() {
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+
+        for (Person person : people) {
+            jdbcTemplate.update("INSERT INTO PEOPLE (name, age, email) VALUES( ?, ?, ?)",
+                    person.getId(), person.getName(), person.getAge(), person.getEmail());
+        }
+
+        long after = System.currentTimeMillis();
+
+        System.out.println(after-before);
+    }
+
+    public void testBatchUpdate() {
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO PEOPLE (name, age, email) VALUES( ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                        preparedStatement.setString(1, people.get(i).getName());
+                        preparedStatement.setInt(2, people.get(i).getAge());
+                        preparedStatement.setString(3, people.get(i).getEmail());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return people.size();
+                    }
+                });
+
+        long after = System.currentTimeMillis();
+
+        System.out.println(after-before);
+    }
+
+    private List<Person> create1000People() {
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i< 1000; i++) {
+            people.add(new Person(i, "TestMan" + i, i*10, String.format("testman%s@gmail.com", i)));
+        }
+
+        return people;
+    }
+}
